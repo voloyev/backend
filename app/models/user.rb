dfbbc84db336c328ed391afe554e363e552ac569
ihortok <itokaryk@gmail.com>
2019-07-02 18:43:18 +0300

    2 new tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
 
   def info(id)
     @user = User.find(id)
+    return @user.first_name if @user.last_name == nil
+
+    return @user.last_name if @user.first_name == nil
+
     @user.first_name + ' ' + @user.last_name
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,19 @@ RSpec.describe User, type: :model do
 
       expect(user.info(id)).to eq(user.first_name + ' ' + user.last_name)
     end
-  end
 
+    it "generates info message with first name only" do
+      user = User.create(first_name: 'Mark')
+      id = user.id
+
+      expect(user.info(id)).to eq(user.first_name)
+    end
+
+    it "generates info message with last name only" do
+      user = User.create(last_name: 'Smith')
+      id = user.id
+
+      expect(user.info(id)).to eq(user.last_name)
+    end
+  end
 end
